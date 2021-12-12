69a283eef854af48ee7c10423b34b6be2196715e
Shailesh Vishwakarma <81180156+Shailesh-python@users.noreply.github.com>
2021-12-12 19:50:57 +0530

    Update 57 Questions Solution-Advanced Problems.sql

--- 57 Questions Solution-Advanced Problems.sql
+++ 57 Questions Solution-Advanced Problems.sql
@@ -22,3 +22,68 @@ inner join cte
 		and ordercount>=1
 where year(o.OrderDate) = 2016
 
+--33. High-value customers - total orders
+select 
+	o.CustomerID,
+	c.CompanyName,
+	sum(od.Quantity*od.UnitPrice) as totalvalue,
+	count(*) as totalorders
+from dbo.orderdetails od
+left join dbo.orders o
+	on od.orderid=o.OrderID
+left join dbo.customers c
+	on o.CustomerID=c.CustomerID
+where year(o.OrderDate)=2016
+group by o.CustomerID,c.CompanyName
+having sum(od.Quantity*od.UnitPrice)>=15000
+
+--34. High-value customers - with discount
+select 
+	o.CustomerID,
+	c.CompanyName,
+	sum(od.Quantity*od.UnitPrice*(1-od.Discount)) as totalvalue
+from dbo.orderdetails od
+left join dbo.orders o
+	on od.orderid=o.OrderID
+left join dbo.customers c
+	on o.CustomerID=c.CustomerID
+where year(o.OrderDate)=2016
+group by o.CustomerID,c.CompanyName
+having sum(od.Quantity*od.UnitPrice*(1-od.Discount))>=10000
+
+
+--35. Month-end orders
+
+select
+	EmployeeID,
+	orderid,
+	OrderDate
+from orders
+where orderdate=EOMONTH(orderdate)
+order by EmployeeID,OrderID
+
+--36. Orders with many line items
+select 
+	top 10
+	orderid,
+	count(distinct productid) TotalOrderDetails
+from OrderDetails
+group by orderid
+order by TotalOrderDetails desc
+
+--37. Orders - random assortment
+
+select 
+	top 2 percent
+	orderid
+from Orders
+order by newid()
+
+--38. Orders - accidental double-entry
+
+select 
+	orderid
+from OrderDetails
+where Quantity>=60
+group by orderid,Quantity
+having count(*)>1
