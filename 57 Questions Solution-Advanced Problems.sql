39719f3236485356dd3527e0a35d18f7b65ca556
Shailesh Vishwakarma <81180156+Shailesh-python@users.noreply.github.com>
2021-12-15 22:22:26 +0530

    Update 57 Questions Solution-Advanced Problems.sql

--- 57 Questions Solution-Advanced Problems.sql
+++ 57 Questions Solution-Advanced Problems.sql
@@ -87,3 +87,240 @@ from OrderDetails
 where Quantity>=60
 group by orderid,Quantity
 having count(*)>1
+
+--39 Orders - accidental double-entry details
+
+select * from dbo.OrderDetails
+where orderid in 
+(select 
+	orderid
+from OrderDetails
+where Quantity>=60
+group by orderid,Quantity
+having count(*)>1)
+
+--40. Orders - accidental double-entry details, derived table
+
+Select
+ distinct
+ OrderDetails.OrderID
+ ,ProductID
+ ,UnitPrice
+ ,Quantity
+ ,Discount
+From OrderDetails 
+ Join (
+ Select
+ OrderID 
+ From OrderDetails
+ Where Quantity >= 60
+ Group By OrderID, Quantity
+ Having Count(*) > 1
+ ) PotentialProblemOrders
+ on PotentialProblemOrders.OrderID = OrderDetails.OrderID
+Order by OrderID, ProductID
+
+--41. Late orders
+
+select 
+	Orderid,
+	OrderDate,
+	RequiredDate,
+	ShippedDate
+from Orders
+where ShippedDate>RequiredDate
+order by orderid
+
+--42. Late orders - which employees?
+
+select 
+	o.EmployeeID,
+	e.LastName,
+	TotalLateOrders=count(o.employeeid)
+from dbo.Orders o
+left join dbo.Employees e
+	on o.EmployeeID=e.EmployeeID
+where o.ShippedDate>o.RequiredDate
+group by o.EmployeeID,e.LastName
+
+--43. Late orders vs. total orders
+;with lateorders as 
+(
+select 
+	o.EmployeeID,
+	TotalLateOrders=count(o.employeeid)
+from dbo.Orders o
+where o.ShippedDate>o.RequiredDate
+group by o.EmployeeID
+),totalorders as 
+(
+select 
+	o.EmployeeID,
+	e.LastName,
+	TotalOrders=count(o.employeeid)
+from dbo.Orders o
+left join dbo.Employees e
+	on o.EmployeeID=e.EmployeeID
+group by o.EmployeeID,e.LastName
+)
+	select 
+	t.EmployeeID,
+	t.LastName,
+	t.TotalOrders,
+	TotalLateOrders=l.TotalLateOrders
+	from totalorders t
+	inner join lateorders l
+	on t.EmployeeID=l.EmployeeID
+
+--44. Late orders vs. total orders - missing employee
+
+;with lateorders as 
+(
+select 
+	o.EmployeeID,
+	TotalLateOrders=count(o.employeeid)
+from dbo.Orders o
+where o.ShippedDate>o.RequiredDate
+group by o.EmployeeID
+),totalorders as 
+(
+select 
+	o.EmployeeID,
+	e.LastName,
+	TotalOrders=count(o.employeeid)
+from dbo.Orders o
+left join dbo.Employees e
+	on o.EmployeeID=e.EmployeeID
+group by o.EmployeeID,e.LastName
+)
+	select 
+	t.EmployeeID,
+	t.LastName,
+	t.TotalOrders,
+	TotalLateOrders=l.TotalLateOrders
+	from totalorders t
+	left join lateorders l
+	on t.EmployeeID=l.EmployeeID
+
+--45. Late orders vs. total orders - fix null
+
+;with lateorders as 
+(
+select 
+	o.EmployeeID,
+	TotalLateOrders=count(o.employeeid)
+from dbo.Orders o
+where o.ShippedDate>o.RequiredDate
+group by o.EmployeeID
+),totalorders as 
+(
+select 
+	o.EmployeeID,
+	e.LastName,
+	TotalOrders=count(o.employeeid)
+from dbo.Orders o
+left join dbo.Employees e
+	on o.EmployeeID=e.EmployeeID
+group by o.EmployeeID,e.LastName
+)
+	select 
+	t.EmployeeID,
+	t.LastName,
+	t.TotalOrders,
+	TotalLateOrders=isnull(l.TotalLateOrders,0)
+	from totalorders t
+	left join lateorders l
+	on t.EmployeeID=l.EmployeeID
+
+--46. Late orders vs. total orders - percentage
+
+;with lateorders as 
+(
+select 
+	o.EmployeeID,
+	TotalLateOrders=count(o.employeeid)
+from dbo.Orders o
+where o.ShippedDate>o.RequiredDate
+group by o.EmployeeID
+),totalorders as 
+(
+select 
+	o.EmployeeID,
+	e.LastName,
+	TotalOrders=count(o.employeeid)
+from dbo.Orders o
+left join dbo.Employees e
+	on o.EmployeeID=e.EmployeeID
+group by o.EmployeeID,e.LastName
+)
+	select 
+	t.EmployeeID,
+	t.LastName,
+	t.TotalOrders,
+	TotalLateOrders=isnull(l.TotalLateOrders,0),
+	PercentLateOrders=isnull(l.TotalLateOrders,0)*1.0000/t.TotalOrders
+	from totalorders t
+	left join lateorders l
+	on t.EmployeeID=l.EmployeeID
+
+--47. Late orders vs. total orders - fix decimal
+
+;with lateorders as 
+(
+select 
+	o.EmployeeID,
+	TotalLateOrders=count(o.employeeid)
+from dbo.Orders o
+where o.ShippedDate>o.RequiredDate
+group by o.EmployeeID
+),totalorders as 
+(
+select 
+	o.EmployeeID,
+	e.LastName,
+	TotalOrders=count(o.employeeid)
+from dbo.Orders o
+left join dbo.Employees e
+	on o.EmployeeID=e.EmployeeID
+group by o.EmployeeID,e.LastName
+)
+	select 
+	t.EmployeeID,
+	t.LastName,
+	t.TotalOrders,
+	TotalLateOrders=isnull(l.TotalLateOrders,0),
+	PercentLateOrders=convert(decimal(10,2),isnull(l.TotalLateOrders,0)*1.0000/t.TotalOrders,2)
+	from totalorders t
+	left join lateorders l
+	on t.EmployeeID=l.EmployeeID
+
+--48. Customer grouping
+;with totalvalue as
+(
+select 
+	od.OrderID,
+	sum(od.Quantity*od.UnitPrice) as TotalOrderValue
+from dbo.orderdetails od
+group by od.OrderID
+),cte_final as
+(
+select 
+	o.CustomerID,
+	c.CompanyName,
+	sum(tv.totalordervalue) as totalvalue
+from Orders o
+left join dbo.Customers c on o.CustomerID = c.CustomerID
+left join totalvalue tv on o.orderid=tv.OrderID
+where year(o.OrderDate) <> 2016
+group by o.CustomerID,c.CompanyName
+)
+	select 
+		*,
+		customergroup=
+			case when totalvalue <=1000 then 'Low'
+				when totalvalue between 1001 and 5000 then 'Medium'
+				else 'High' end 
+
+	from cte_final
+
+--49. Customer grouping - fix null
