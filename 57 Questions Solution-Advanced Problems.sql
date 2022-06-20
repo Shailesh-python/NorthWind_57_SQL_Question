86becf5b438cd5e0b6487807c6b0e472c6a2989d
Shailesh Vishwakarma <81180156+Shailesh-python@users.noreply.github.com>
2022-06-20 12:50:11 +0530

    Update 57 Questions Solution-Advanced Problems.sql

--- 57 Questions Solution-Advanced Problems.sql
+++ 57 Questions Solution-Advanced Problems.sql
@@ -324,3 +324,119 @@ group by o.CustomerID,c.CompanyName
 	from cte_final
 
 --49. Customer grouping - fix null
+--50. Customer grouping with percentage
+
+;with totalvalue as
+(
+select 
+	od.OrderID,
+	sum(od.Quantity*od.UnitPrice) as TotalOrderValue
+from dbo.orderdetails od
+group by od.OrderID
+),cte_final as
+(
+select 
+	o.CustomerID,
+	c.CompanyName,
+	sum(tv.totalordervalue) as totalvalue
+from Orders o
+left join dbo.Customers c on o.CustomerID = c.CustomerID
+left join totalvalue tv on o.orderid=tv.OrderID
+where year(o.OrderDate) <> 2016
+group by o.CustomerID,c.CompanyName
+),cte_final2 as
+	(
+	select 
+		*,
+		customergroup=
+			case when totalvalue <=1000 then 'Low'
+				when totalvalue between 1001 and 5000 then 'Medium'
+				else 'High' end 
+	from cte_final
+	)	
+		select  
+			r.customergroup,
+			Count(r.customergroup) as  TotalInGroup
+		from cte_final2 As R
+		group by r.customergroup
+		order by TotalInGroup desc
+
+--52. Countries with suppliers or customers
+
+select Country from dbo.Suppliers
+Union
+select Country from dbo.Customers
+
+--53. Countries with suppliers or customers, version 2
+
+select
+	Distinct
+	s.Country as SupplierCountry,
+	c.Country as CustomerCountry
+from dbo.Suppliers S
+Full Join dbo.Customers C 
+	On S.Country = C.Country
+
+--54. Countries with suppliers or customers - version 3
+
+;WITH T1 AS 
+(Select Country , COUNT(1) as CustomerCount From
+(select Distinct CustomerID,Country from dbo.Customers) as A
+Group by Country)	
+,T2 AS 
+(
+Select Country , COUNT(1) as SupplierCount From
+(select Distinct SupplierID,Country from dbo.Suppliers) as B
+Group by Country)
+,T3 AS 
+(
+select Country from dbo.Suppliers
+Union
+select Country from dbo.Customers
+)
+	SELECT 
+		T3.Country,
+		ISNULL(T1.CustomerCount,0) as CustomerCount,
+		ISNULL(T2.SupplierCount,0) as SupplierCount
+
+	FROM T3
+	LEFT JOIN T1 ON T3.Country = T1.Country
+	LEFT JOIN T2 ON T3.Country = T2.Country
+
+--55. First order in each country
+
+select 
+	distinct
+	ShipCountry,
+	FIRST_VALUE(orderdate) over ( partition by shipcountry order by orderdate asc) as FirstOrderdate,
+	FIRST_VALUE(OrderID) over ( partition by shipcountry order by orderdate asc) as FirstOrderid,
+	FIRST_VALUE(CustomerID) over ( partition by shipcountry order by orderdate asc) as FirstOrderCustomerId
+from dbo.Orders
+order by ShipCountry asc
+
+
+--56. Customers with multiple orders in 5 day period
+;With MainTable As
+(
+Select
+	o.CustomerID,
+	o.Orderid,
+	o.OrderDate
+From dbo.Orders o
+),Derived_Table As
+(
+	Select 
+		MainTable.CustomerID,
+		MainTable.OrderID as FirstOrderID,
+		MainTable.Orderdate as FirstOrderDate,
+		LEAD(MainTable.OrderID,1) OVER (PARTITION BY MainTable.CUSTOMERID ORDER BY MainTable.CUSTOMERID ,MainTable.ORDERDATE ASC) as SecondOrderID,
+		LEAD(MainTable.OrderDate,1) OVER (PARTITION BY MainTable.CUSTOMERID ORDER BY MainTable.CUSTOMERID ,MainTable.ORDERDATE ASC) as SecondOrderDate
+		
+	From MainTable
+	--Order By CustomerID,OrderID,OrderDate
+)
+	SELECT * ,NumberOfDays = DATEDIFF(D,FirstOrderDate,SecondOrderDate)
+	FROM Derived_Table
+	WHERE NOT SecondOrderDate IS NULL
+		AND DATEDIFF(D,FirstOrderDate,SecondOrderDate) <=5
+
